660cea2beb03327e4af52005337aaef4c9295b99
Samagra <samagra14@gmail.com>
2017-07-19 01:56:21 +0530

    Notes repository changes.

--- app/src/main/java/com/example/samagra/notepad/data/NotesRepository.java
+++ app/src/main/java/com/example/samagra/notepad/data/NotesRepository.java
@@ -1,8 +1,28 @@
 package com.example.samagra.notepad.data;
 
+import android.database.Cursor;
+import android.support.annotation.NonNull;
+
 /**
  * Created by samagra on 19/7/17.
+ * Main entry point for accessing notes data.
  */
 
 public interface NotesRepository {
+
+    interface LoadNotesCallback{
+        void onNotesLoaded(Cursor cursor);
+    }
+
+    interface GetNoteCallback{
+        void onNoteLoaded(Note note);
+    }
+
+    void getNotes(@NonNull LoadNotesCallback callback);
+
+    void getNote(@NonNull String noteId, @NonNull GetNoteCallback callback);
+
+    void saveNote(@NonNull Note note);
+
+    void refreshData();
 }
